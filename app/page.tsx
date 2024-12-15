32e7c786b7734612b7fee15a069563f7353c9380
zzzzshawn <zzzzshawn@gmail.com>
2024-12-15 16:11:00 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="z-[99999] border w-full flex items-center justify-center h-screen">
+     
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        modernbold: ["var(--font-modern-bold)", "sans-serif"],
+        modernmono: ["var(--font-modern-mono)", "sans-serif"],
+        modernreg: ["var(--font-modern-reg)", "sans-serif"],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
