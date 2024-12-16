f719af6f3d57151845fedf65626f89528e590235
zzzzshawn <zzzzshawn@gmail.com>
2024-12-17 03:55:52 +0530

    alr gyatt done

--- components/Github.tsx
+++ components/Github.tsx
@@ -37,15 +37,43 @@ const Github = async () => {
           followers={userStats.Followers}
           classname="col-start-3 col-end-5 row-start-4 row-end-5"
         />
-        <Repos repos={userStats.Repositories} classname="col-start-5 col-end-9 row-start-1 row-end-2" />
-        <Commit commits={userStats["Total Contibutions"]} classname="col-start-5 col-end-7 row-start-2 row-end-5" />
-        <PRs pr={userStats["Pull Requests"]} classname="col-start-7 col-end-8 row-start-2 row-end-4" />
-        <ContributedTo contros={userStats["Contributed To"]} classname="col-start-7 col-end-9 row-start-4 row-end-5" />
-        <Issues issues={userStats.Issues}  classname="col-start-8 col-end-9 row-start-2 row-end-4" />
+        <Repos
+          repos={userStats.Repositories}
+          classname="col-start-5 col-end-9 row-start-1 row-end-2"
+        />
+        <Commit
+          commits={userStats["Total Contibutions"]}
+          classname="col-start-5 col-end-7 row-start-2 row-end-5"
+        />
+        <PRs
+          pr={userStats["Pull Requests"]}
+          classname="col-start-7 col-end-8 row-start-2 row-end-4"
+        />
+        <ContributedTo
+          contros={userStats["Contributed To"]}
+          classname="col-start-7 col-end-9 row-start-4 row-end-5"
+        />
+        <Issues
+          issues={userStats.Issues}
+          classname="col-start-8 col-end-9 row-start-2 row-end-4"
+        />
         <Stars
           stars={userStats["Star Earned"]}
           classname="col-start-1 col-end-3 row-start-3 row-end-5"
         />
+      </div>
+      <div className=" p-2 rounded-2xl max-w-3xl mx-auto mt-2 flex flex-col relative">
+        <div className="">
+          <h1 className="max-w-2xl mx-auto font-modernbold text-2xl mb-2">
+            Contribution Graph:
+          </h1>
+        </div>
+        <div
+          className="max-w-2xl bg-zinc-900/60 backdrop-blur-xl border border-zinc-200/20 backdrop-saturate-200 p-3 rounded-2xl mx-auto"
+          dangerouslySetInnerHTML={{
+            __html: graph.graph,
+          }}
+        ></div>
       </div>
     </div>
   );
