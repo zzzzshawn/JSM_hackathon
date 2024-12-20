94745012833b3dfd0183920528d856d3f27845e6
zzzzshawn <zzzzshawn@gmail.com>
2024-12-21 03:44:42 +0530

    ..

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,9 @@
-
+"use client"
 import Github from "@/components/Github";
 import Profile from "@/components/Profile";
 import Projects from "@/components/Projects";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="z-[99] flex flex-col items-start justify-start py-20 min-h-screen font-modernreg max-lg:px-3 max-w-6xl w-full  max-lg:w-full mx-auto relative overflow-hidden">
      <Profile />
